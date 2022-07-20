602965808779e30e7ad92e570b819245ad633a04
TaleaX <sophie.dehne01@gmail.com>
2022-07-20 13:00:28 +0200

    add libft

---
+++ libft_printf/ft_printf/libft_funcs.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft_funcs.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tdehne <tdehne@student.42heilbronn.de>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/27 17:56:48 by tdehne            #+#    #+#             */
+/*   Updated: 2022/04/27 17:57:08 by tdehne           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+size_t	ft_strlen(const char *str)
+{
+	size_t	counter;
+
+	counter = 0;
+	while (str[counter] != '\0')
+		counter++;
+	return (counter);
+}
